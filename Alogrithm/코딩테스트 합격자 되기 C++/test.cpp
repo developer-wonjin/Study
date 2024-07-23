e9101dda30a741f1cf7be8cd4d2f1cc545f4d639
wonjin1776 <wonjin1776@naver.com>
2024-07-23 22:41:16 +0900

    update

--- Alogrithm/코딩테스트 합격자 되기 C++/test.cpp
+++ Alogrithm/코딩테스트 합격자 되기 C++/test.cpp
@@ -1,10 +1,45 @@
+#include <iostream>
 #include <string>
-#include <vector>
+#include <stack>
 
 using namespace std;
 
-int solution(string s) {
-    
-    
+int len;
+int res;
 
+bool notMatch(char ele, char ch) {
+    if (ele == '(' && ch == ')') return false;
+    if (ele == '[' && ch == ']') return false;
+    if (ele == '{' && ch == '}') return false;
+    return true;
+}
+
+void solve(string &newS) {
+    stack<char> stk;
+    
+    for (int j = 0; j < len; j++) {
+        char newCh = newS[j];
+        if (newCh == '(' || newCh == '[' || newCh == '{') {
+            stk.push(newCh);
+        } else if (stk.empty() || notMatch(stk.top(), newCh)) {
+            return;
+        } else {
+            stk.pop();
+        }
+    }
+
+    if (stk.size() == 0) {
+        res++;
+    }
+}
+
+int solution(string s) {
+    len = s.length();
+    
+    for (int i = 0; i < len; i++) {
+        s = s.substr(1) + s[0];
+        solve(s);
+    }
+
+    return res;
 }
