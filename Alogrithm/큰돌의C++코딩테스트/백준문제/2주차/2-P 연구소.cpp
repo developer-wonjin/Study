c0fe488285cc74716693f862b786e58b26b488da
wonjin1776 <wonjin1776@naver.com>
2024-12-19 18:38:28 +0900

    update

--- Alogrithm/큰돌의C++코딩테스트/백준문제/2주차/2-P 연구소.cpp
+++ Alogrithm/큰돌의C++코딩테스트/백준문제/2주차/2-P 연구소.cpp
@@ -5,11 +5,15 @@
 시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
 2 초	512 MB	110258	63969	35847	55.306%
 문제
-인체에 치명적인 바이러스를 연구하던 연구소에서 바이러스가 유출되었다. 다행히 바이러스는 아직 퍼지지 않았고, 바이러스의 확산을 막기 위해서 연구소에 벽을 세우려고 한다.
 
-연구소는 크기가 N×M인 직사각형으로 나타낼 수 있으며, 직사각형은 1×1 크기의 정사각형으로 나누어져 있다. 연구소는 빈 칸, 벽으로 이루어져 있으며, 벽은 칸 하나를 가득 차지한다. 
+연구소는 크기가 N×M인 직사각형으로 나타낼 수 있으며, 직사각형은 1×1 크기의 정사각형으로 나누어져 있다. 
 
-일부 칸은 바이러스가 존재하며, 이 바이러스는 상하좌우로 인접한 빈 칸으로 모두 퍼져나갈 수 있다. 새로 세울 수 있는 벽의 개수는 3개이며, 꼭 3개를 세워야 한다.
+빈 칸, 
+벽
+
+바이러스는 상하좌우로 인접한 빈 칸으로 모두 퍼져나갈 수 있다. 
+새로 세울 수 있는 벽의 개수는 3개, 
+꼭 3개를 세워야 한다.
 
 예를 들어, 아래와 같이 연구소가 생긴 경우를 살펴보자.
 
@@ -20,7 +24,10 @@
 0 0 0 0 0 1 1
 0 1 0 0 0 0 0
 0 1 0 0 0 0 0
-이때, 0은 빈 칸, 1은 벽, 2는 바이러스가 있는 곳이다. 아무런 벽을 세우지 않는다면, 바이러스는 모든 빈 칸으로 퍼져나갈 수 있다.
+
+0은 빈 칸, 
+1은 벽, 
+2는 바이러스
 
 2행 1열, 1행 2열, 4행 6열에 벽을 세운다면 지도의 모양은 아래와 같아지게 된다.
 
@@ -31,6 +38,7 @@
 0 0 0 0 0 1 1
 0 1 0 0 0 0 0
 0 1 0 0 0 0 0
+
 바이러스가 퍼진 뒤의 모습은 아래와 같아진다.
 
 2 1 0 0 1 1 2
@@ -42,17 +50,8 @@
 0 1 0 0 0 0 0
 벽을 3개 세운 뒤, 바이러스가 퍼질 수 없는 곳을 안전 영역이라고 한다. 위의 지도에서 안전 영역의 크기는 27이다.
 
-연구소의 지도가 주어졌을 때 얻을 수 있는 안전 영역 크기의 최댓값을 구하는 프로그램을 작성하시오.
+안전 영역 크기의 최댓값을 구하는 프로그램을 작성하시오.
 
-입력
-첫째 줄에 지도의 세로 크기 N과 가로 크기 M이 주어진다. (3 ≤ N, M ≤ 8)
-
-둘째 줄부터 N개의 줄에 지도의 모양이 주어진다. 0은 빈 칸, 1은 벽, 2는 바이러스가 있는 위치이다. 2의 개수는 2보다 크거나 같고, 10보다 작거나 같은 자연수이다.
-
-빈 칸의 개수는 3개 이상이다.
-
-출력
-첫째 줄에 얻을 수 있는 안전 영역의 최대 크기를 출력한다.
 
 예제 입력 1 
 7 7
@@ -86,3 +85,99 @@
 예제 출력 3 
 3
 */
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+/*
+입력
+첫째 줄에 지도의 세로 크기 N과 가로 크기 M이 주어진다. (3 ≤ N, M ≤ 8)
+
+둘째 줄부터 N개의 줄에 지도의 모양이 주어진다. 
+0은 빈 칸, 
+1은 벽, 
+2는 바이러스가 있는 위치이다. 
+2의 개수는 2보다 크거나 같고, 10보다 작거나 같은 자연수이다.
+
+빈 칸의 개수는 3개 이상이다.
+
+출력
+첫째 줄에 얻을 수 있는 안전 영역의 최대 크기를 출력한다.
+*/
+
+int N, M;
+vector<vector<int>> myMap(8, vector<int>(8, 0));
+vector<pair<int, int>> emptyV, virus;
+
+int dy[4] = {-1, 0, 1, 0};
+int dx[4] = {0, 1, 0, -1};
+int ans = 0;
+
+int main() {
+
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> myMap[i][j];
+            if (myMap[i][j] == 0) emptyV.push_back({i, j});
+            else if (myMap[i][j] == 2) virus.push_back({i, j});
+        }
+    }
+
+    int emptyVN = emptyV.size();
+
+    for (int i = 0; i < emptyVN; i++) {
+        for (int j = i + 1; j < emptyVN; j++) {
+            for (int k = j + 1; k < emptyVN; k++) {
+                
+                queue<pair<int, int>> q;
+                vector<vector<int>> tmpMap = myMap;
+                vector<vector<int>> visited(8, vector<int>(8, 0));
+                
+                // 벽을 세우자
+                tmpMap[emptyV[i].first][emptyV[i].second] = 1;
+                tmpMap[emptyV[j].first][emptyV[j].second] = 1;
+                tmpMap[emptyV[k].first][emptyV[k].second] = 1;
+                
+                // 바이러스를 퍼트리자                 
+                for (auto& ele : virus) {
+                    visited[ele.first][ele.second] = 1;
+                    tmpMap[ele.first][ele.second] = 2;
+                    q.push(ele);
+                }
+
+                while (q.size()) {
+                    pair<int, int> curr = q.front(); q.pop();
+
+                    for (int dir = 0; dir < 4; dir++) {
+                        int ny = curr.first + dy[dir];
+                        int nx = curr.second + dx[dir];
+
+                        if (ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
+                        if (tmpMap[ny][nx] == 1) continue;
+                        if (visited[ny][nx])continue;
+
+                        visited[ny][nx] = 1;
+                        tmpMap[ny][nx] = 2;
+                        q.push({ny, nx});
+                    }
+                }
+
+                int tot = 0;
+                for (int i = 0; i < N; i++) {
+                    for (int j = 0; j < M; j++) {
+                        if (tmpMap[i][j] == 0) tot++;
+                    }
+                }
+                ans = max(ans, tot);
+            }
+        }
+    }
+
+    cout << ans << "\n";
+    return 0;
+}
---
+++ Alogrithm/큰돌의C++코딩테스트/백준문제/2주차/2-Q 치즈(희성님풀이).cpp
@@ -0,0 +1,112 @@
+// https://www.acmicpc.net/problem/2636
+/*
+치즈
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+1 초	128 MB	33900	19200	13710	55.448%
+문제
+아래 <그림 1>과 같이 정사각형 칸들로 이루어진 사각형 모양의 판이 있고, 
+치즈 : 회색
+판의 가장자리(<그림 1>에서 네모 칸에 X친 부분)에는 치즈가 놓여 있지 않으며 치즈에는 하나 이상의 구멍이 있을 수 있다.
+공기와 접촉된 칸은 '한 시간'이 지나면 녹아 없어진다.
+치즈의 구멍 속에는 공기가 없지만 구멍을 둘러싼 치즈가 녹아서 '구멍이 열리면 구멍 속으로 공기가 들어가게 된다'
+<그림 1>의 경우, 치즈의 구멍을 둘러싼 치즈는 녹지 않고 ‘c’로 표시된 부분만 한 시간 후에 녹아 없어져서 <그림 2>와 같이 된다.
+<그림 3>은 원래 치즈의 두 시간 후 모양을 나타내고 있으며, 남은 조각들은 한 시간이 더 지나면 모두 녹아 없어진다. 그러므로 처음 치즈가 모두 녹아 없어지는 데는 세 시간이 걸린다. <그림 3>과 같이 치즈가 녹는 과정에서 여러 조각으로 나누어 질 수도 있다.
+입력으로 사각형 모양의 판의 크기와 한 조각의 치즈가 판 위에 주어졌을 때, 
+시간: 공기 중에서 치즈가 모두 녹아 없어지는 데 걸리는 시간 
+칸 갯수: 모두 녹기 한 시간 전에 남아있는 치즈조각이 놓여 있는 칸
+입력
+첫째 줄에는 사각형 모양 판의 세로와 가로의 길이가 양의 정수로 주어진다. 
+세로와 가로의 길이는 최대 100이다. 
+판의 각 가로줄의 모양이 윗 줄부터 차례로 둘째 줄부터 마지막 줄까지 주어진다. 
+치즈가 없는 칸은 0,
+치즈가 있는 칸은 1
+출력
+첫째 줄에는 치즈가 모두 녹아서 없어지는 데 걸리는 시간을 출력하고, 둘째 줄에는 모두 녹기 한 시간 전에 남아있는 치즈조각이 놓여 있는 칸의 개수를 출력한다.
+예제 입력 1 
+13 12
+0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 1 1 0 0 0
+0 1 1 1 0 0 0 1 1 0 0 0
+0 1 1 1 1 1 1 0 0 0 0 0
+0 1 1 1 1 1 0 1 1 0 0 0
+0 1 1 1 1 0 0 1 1 0 0 0
+0 0 1 1 0 0 0 1 1 0 0 0
+0 0 1 1 1 1 1 1 1 0 0 0
+0 0 1 1 1 1 1 1 1 0 0 0
+0 0 1 1 1 1 1 1 1 0 0 0
+0 0 1 1 1 1 1 1 1 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0
+예제 출력 1 
+3
+5
+*/
+# include<bits/stdc++.h>
+using namespace std;
+int n, m;
+const int NMAX = 100;
+const int MMAX = 100;
+vector<vector<int>> myMap(NMAX, vector<int>(MMAX, 0));
+int dy[4] = {-1, 0, 1, 0};
+int dx[4] = {0, 1, 0, -1};
+int main() {
+    ios::sync_with_stdio(0);cin.tie(0);
+    cin >> n >> m;
+    int currCheeze = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> myMap[i][j];
+            currCheeze += myMap[i][j];
+        }
+    }
+    int t = 0;
+    int beforeCheeze = 0;
+    while(currCheeze) {
+        // 녹이자
+        vector<pair<int,int>> meltList;
+        queue<pair<int, int>> q;
+        vector<vector<int>> visited(n, vector<int>(m, 0));
+        // 공기를 BFS하자
+        visited[0][0] = 1;
+        q.push({0, 0});
+        while(q.size()) {
+            pair<int, int> curr = q.front(); q.pop();
+            int y = curr.first;
+            int x = curr.second;
+            for (int dir = 0; dir < 4; dir++) {
+                int ny = y + dy[dir];
+                int nx = x + dx[dir];
+                if (ny < 0 || ny >= n || nx < 0 || nx >= m)continue;
+                if (myMap[ny][nx] == 1) {
+                    meltList.push_back({ny, nx});
+                    continue; // 공기만 bfs할꺼임
+                } 
+                if (visited[ny][nx]) continue;
+                visited[ny][nx] = 1;
+                q.push({ny, nx});
+            }
+        }
+        for (auto& pos : meltList) myMap[pos.first][pos.second] = 0;
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < m; j++) {
+        //         cout << myMap[i][j] << " ";
+        //     }
+        //     cout << "\n";
+        // }
+        // cout << "\n";
+        // cout << "\n";
+            
+        t++;
+        // 녹은후 현재 currCheeze갯수 재산출
+        beforeCheeze = currCheeze;
+        currCheeze = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++){
+                if (myMap[i][j]) currCheeze++;
+            }
+        }
+    }
+    cout << t << "\n";
+    cout << beforeCheeze << "\n";
+    return 0;
+}
